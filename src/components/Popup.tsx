97898f6313bc1b591ac47439064a441f20092786
ustuncem <ustuncem7@gmail.com>
2025-08-02 12:07:32 +0300

    sync

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,64 +1,68 @@
-import {useState} from 'preact/hooks';
+import { useState } from "preact/hooks";
 
 export default function Popup() {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
-  const handleOpen = () => setIsOpen(prev => !prev);
+  const handleOpen = () => setIsOpen((prev) => !prev);
 
   const handleChildWrapperClick = (e: MouseEvent) => e.stopPropagation();
+
   return (
-    isOpen && (
-      <div
-        className="fixed bg-black/40 inset-0 z-10 px-4 grid place-items-center cursor-pointer"
+    <>
+      <button
+        class="bg-primary hover:text-primary hover:border-primary my-2 cursor-pointer rounded-full border px-6 py-2 text-xl text-white transition-colors hover:bg-white"
         onClick={handleOpen}
       >
-        <article
-          className="max-w-sm mx-auto w-full bg-white p-4 lg:max-w-lg relative z-20 cursor-default"
-          onClick={handleChildWrapperClick}
+        Loca Bilgileri
+      </button>
+      {isOpen && (
+        <div
+          className="fixed inset-0 z-10 grid cursor-pointer place-items-center bg-black/40 px-4"
+          onClick={handleOpen}
         >
-          <h1 className="text-lg sm:text-xl mb-4 text-primary-500 flex justify-between items-center">
-            Lezzet Dolu Romantik Anlar
-            <button onClick={handleOpen}>
-              <img className="w-4" src="/assets/icons/close.svg" />
-            </button>
-          </h1>
-          <div>
-            <p className="text-base mb-4 flex justify-between">
-              Date Menüsü <span className="text-primary-500">1500 TL</span>
-            </p>
-            <ul className="marker:text-neutral-300 list-inside mb-4 p-2 border border-dashed border-primary-300">
-              <li className="mb-2">
-                <span className="text-primary-500">2</span> Makarna veya{' '}
-                <span className="text-primary-500">2</span> Pizza
-              </li>
-              <li className="mb-2">
-                <span className="text-primary-500">2</span> Kokteyl veya{' '}
-                <span className="text-primary-500">1</span> Şişe Şarap
-              </li>
-              <li>Meyve ve Peynir Tabağı</li>
-            </ul>
-            <ul className="list-disc list-inside grid grid-cols-2 mb-2 marker:text-neutral-700">
-              <div class="text-[14px]">
-                <p className="mb-2 text-primary-500 text-base">Pizzalar</p>
-                <li className="mb-2">Margarita</li>
-                <li className="mb-2">Alaturka</li>
-                <li className="mb-2">Funghi</li>
-                <li className="mb-2">Tavuk Cheddar</li>
-                <li className="mb-2">Tonbalıklı Pizza</li>
-              </div>
-              <div class="text-[14px]">
-                <p className="mb-2 text-primary-500 text-base">Makarnalar</p>
-                <li className="mb-2">Penne Arabiata</li>
-                <li className="mb-2">Bolonez</li>
-                <li className="mb-2">Fettucini Alfredo </li>
-                <li className="mb-2">Penne Pesto</li>
-                <li className="mb-2">Linguini</li>
-              </div>
-            </ul>
-            <p className="text-xs text-primary-500">Not: Cumartesi hariç her gün geçerlidir</p>
-          </div>
-        </article>
-      </div>
-    )
+          <article
+            className="relative z-20 mx-auto w-full max-w-sm cursor-default bg-white p-4 lg:max-w-lg"
+            onClick={handleChildWrapperClick}
+          >
+            <h1 className="text-primary-500 mb-4 flex items-center justify-end text-lg sm:text-xl">
+              <button onClick={handleOpen}>
+                <img className="w-4" src="/assets/icons/close.svg" />
+              </button>
+            </h1>
+            <div>
+              <p className="mb-4 justify-between text-center text-3xl font-semibold">
+                LOCA <span className="font-light"></span>
+              </p>
+              <ul className="mb-2 list-inside p-2 marker:text-neutral-300">
+                <li className="border-primary-light mb-2 flex justify-between gap-6 border-b pb-2">
+                  <div className="text-xl">
+                    KİŞİ BAŞI HARCAMA LİMİTİ /{" "}
+                    <span className="font-semibold">HAFTA İÇİ</span>
+                  </div>
+                  <div className="text-lg">
+                    <span className="font-semibold text-nowrap">3.000 TL</span>
+                  </div>
+                </li>
+                <li className="border-primary-light mb-2 flex justify-between gap-6 border-b pb-2">
+                  <div className="text-xl">
+                    KİŞİ BAŞI HARCAMA LİMİTİ /{" "}
+                    <span className="font-semibold">HAFTA SONU</span>
+                  </div>
+                  <div className="text-lg">
+                    <span className="font-semibold text-nowrap">3.500 TL</span>
+                  </div>
+                </li>
+              </ul>
+              <p className="mb-3 text-center text-lg">
+                Loca kapasitesi en fazla 6 kişi ile sınırlıdır.
+              </p>
+              <p className="text-primary-500 text-center text-base font-light">
+                Fiyatlarımıza giriş ücreti ve shuttle hizmetimiz dahildir.
+              </p>
+            </div>
+          </article>
+        </div>
+      )}
+    </>
   );
 }
